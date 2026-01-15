b004467182ae3484559383252a6338258f6d32b8
Abdul Rabbani <58230246+abdulrabbani00@users.noreply.github.com>
2026-01-15 08:58:27 -0500

    Update basic_swap.rs

--- examples/basic_swap.rs
+++ examples/basic_swap.rs
@@ -36,7 +36,10 @@ const USDC_TOKEN: &str = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    // Initialize tracing with INFO level by default if RUST_LOG is not set
+    let env_filter = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info"));
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
 
     tracing::info!("Starting Solana DEX SuperAgg Examples");
 
